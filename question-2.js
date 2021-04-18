a3dab1b41d458db9deb1262ddcd98192e6e77678
Holmfred <fredrik1304@live.no>
2021-04-18 15:00:19 +0200

    Done?

--- question-2.js
+++ question-2.js
@@ -1,16 +1,33 @@
-const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+const url = "https://rawg-video-games-database.p.rapidapi.com/games"
+
+const key = { "headers": {
+    "x-rapidapi-key": "09a0428acamsh48cb2c73b238492p1590c8jsnbbfdc2570bf5",
+    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com"
+}};
+
+const resultsContainer = document.querySelector("body");
 
 
 async function dontKnowYet () {
 
-    const response = await fetch(url);
+    const response = await fetch(url, key);
 
     const data = await response.json();
 
-    const games = data.all;
+    const games = data.results;
 
-    for(let i = 0; i < games.length; i++) {
-        console.log(games[i].text)
+    resultsContainer.innerHTML = "";
+
+    for(let i = 0; i < games.length; i++){
+        console.log(games[i].name, games[i].rating, games[i].tags.length)
+
+        const combined = games[i].name +" "+ games[i].rating +" "+ games[i].tags.length
+
+        if (i === 8){
+            break
+        }
+
+        resultsContainer.innerHTML += `<div>${combined}</div>`;
     }
 }
 
