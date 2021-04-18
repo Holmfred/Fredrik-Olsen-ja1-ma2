acbebb886081389289f03e90a1ec39ef3e0495d0
Holmfred <fredrik1304@live.no>
2021-04-18 03:18:40 +0200

    Where's the KEY?

---
+++ question-2.js
@@ -0,0 +1,17 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+
+
+async function dontKnowYet () {
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    const games = data.all;
+
+    for(let i = 0; i < games.length; i++) {
+        console.log(games[i].text)
+    }
+}
+
+dontKnowYet();
